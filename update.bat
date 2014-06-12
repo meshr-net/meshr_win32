556cf423eb848842c63ecdeb725ea0b5fec1a5e8
Yury <dev@meshr.net>
2014-06-13 02:05:05 +0400

    Fri 06/13/2014  2:05:05

--- update.bat
+++ update.bat
@@ -9,12 +9,13 @@ set PATH=%meshr:/=\%\bin;%meshr:/=\%\usr\bin;%PATH%
 cd %meshr:/=\%
 set GIT_SSL_NO_VERIFY=true
 echo  %DATE% %TIME%
-set tar="tmp\push%TIME::=.%.tar"
+set tar="tmp\push_%TIME::=.%.tar"
+set backup="tmp/backup_%TIME::=.%.tar"
 git status | grep -e "modified:" && git status | grep -e "modified:" | cut -c 14- | tar rf %tar% -v -T - --exclude=etc/* --exclude=www/*.exe --exclude=bin/DualServer.* --exclude=bin/BluetoothView.cfg --ignore-failed-read  --ignore-command-error --overwrite
 IF "%1"=="" IF EXIST  push.bat tar --list --file %tar% | grep "." && goto :end
 type %meshr:/=\%\.git\index.lock && (del %meshr:/=\%\.git\index.lock || goto :end)
 set branch=release
-IF "%1"=="master" set branch=master
+IF "%1"=="master" set branch=master 
 git pull origin %branch% < NUL || ( 
   git config user.email "user@meshr.net"  
   git config user.name "%USERNAME% %USERDOMAIN%"  
@@ -22,6 +23,7 @@ git pull origin %branch% < NUL || (
   rem SSL certificate problem: unable to get local issuer certificate
   git config http.sslVerify false
   git remote set-url origin https://github.com/meshr-net/meshr_win32.git
+  git reset --merge  < NUL
   git commit -am "%USERNAME%.%USERDOMAIN% %DATE% %TIME%"
   git pull origin %branch% < NUL > tmp\git.log 2>&1 || (
       grep "fatal: unable to access" tmp\git.log  && goto :ipkg
@@ -29,12 +31,12 @@ git pull origin %branch% < NUL || (
       git fetch origin %branch%
       type %meshr:/=\%\.git\index.lock && (del %meshr:/=\%\.git\index.lock || goto :end)
       git reset --merge  < NUL
-      tar cf tmp/backup.tar --exclude-vcs --ignore-failed-read  --ignore-command-error -X etc/tarignore etc/* bin/DualServer.ini bin/BluetoothView.cfg 
+      tar cf %backup% --exclude-vcs --ignore-failed-read  --ignore-command-error -X etc/tarignore etc/* bin/DualServer.ini bin/BluetoothView.cfg 
       git reset --hard origin/%branch% < NUL || ( 
         call %meshr:/=\%\bin\services.bat stop
         git reset  --hard  origin/%branch% < NUL
         sleep 9 )
-      tar xf tmp/backup.tar  -C . --overwrite --ignore-failed-read  --ignore-command-error
+      tar xf %backup%  -C . --overwrite --ignore-failed-read  --ignore-command-error
       call %meshr:/=\%\bin\services.bat start
     )
 )
