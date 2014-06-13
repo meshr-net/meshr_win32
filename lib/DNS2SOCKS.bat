573c4c72b6af34960dc2b34d96826314c5a0288f
Yury <dev@meshr.net>
2014-06-13 17:58:12 +0400

    Fri 06/13/2014 17:58:09

--- lib/DNS2SOCKS.bat
+++ lib/DNS2SOCKS.bat
@@ -5,7 +5,7 @@ set PATH=%PATH%;%meshr:/=\%\bin
 start DNS2SOCKS.exe /l:%meshr:/=\%\tmp\dns.log %1:9150 8.8.8.8 10.177.254.1
 netsh interface ip set dns %2 static 10.177.254.1
 if not "%4"=="" route delete 0.0.0.0 mask 0.0.0.0 %4
-for /f "tokens=1 delims=. " %%f in ('route print ^| find "TAP-"') do route add 0.0.0.0 mask 0.0.0.0 10.177.254.2 metric 2 IF %%f
+for /f "tokens=1 delims=. " %%f in ('route print ^| find "TAP-"') do route add 0.0.0.0 mask 0.0.0.0 10.177.254.2 metric 128 IF %%f
 for /f "tokens=*" %%f in ('type %meshr:/=\%\etc\wifi.txt') do set "%%f"
 netsh interface ip set dns "%NetConnectionID%" static 10.177.254.1
 set IPAddress=%3
--- lib/tor-tun.bat
+++ lib/tor-tun.bat
@@ -39,7 +39,7 @@ ipconfig | find "%NetConnectionID%" || wmic path win32_networkadapter where NetC
 netsh interface ip set address "%NetConnectionID%" static 10.177.254.1 255.255.255.0 
 netsh interface ip set dns "%NetConnectionID%" dhcp
 (echo %DefaultIPGateway% | find "10.177." ) || set DefaultIPGateway=%torIP%
-if not "%torIP%"=="127.0.0.1" ( 
+if not "%torIP%"=="127.0.0.1" curl -m 20 --proxy socks5h://%torIP%:9150 http://208.91.199.147 -o NUL && ( 
   start cmd /c "%meshr:/=\%\bin\sleep 5 && %meshr:/=\%\lib\DNS2SOCKS.bat %torIP% "%NetConnectionID%" "%IPAddress%" %DefaultIPGateway%"
   badvpn-tun2socks --tundev "tap0901:%NetConnectionID%:10.177.254.1:10.177.254.0:255.255.255.0" --netif-ipaddr 10.177.254.2 --netif-netmask 255.255.255.0 --socks-server-addr %torIP%:9150
   exit
