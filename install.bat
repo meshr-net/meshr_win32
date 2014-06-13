2d74eeed6e500bbf2f739880b24c51fc901547e9
Yury <dev@meshr.net>
2014-06-13 11:58:17 +0400

    Fri 06/13/2014 11:58:15

--- install.bat
+++ install.bat
@@ -9,7 +9,7 @@ SET mpath=%CD:\=/%
 if "%1"=="" goto blank
 if "%1"=="postinst" goto postinst
 if "%1"=="Uninstall" (
-    if exist %mpath%\var\run\wifi.txt call %mpath%\lib\setip.bat %mpath%\var\run\wifi.txt 
+    if exist %mpath%\var\run\wifi.txt call %mpath%\lib\setip.bat "%mpath%\var\run\wifi.txt"
     call %~dp0\bin\services.bat stop 
     call %~dp0\bin\services.bat remove confirm
     cd %~dp0
