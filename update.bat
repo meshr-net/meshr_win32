d56241a85ca88af2fea1ccf5bd0722c29be12f3e
Yury <dev@meshr.net>
2014-06-23 17:47:57 +0400

    Mon 06/23/2014 17:47:52

--- update.bat
+++ update.bat
@@ -13,8 +13,8 @@ set t=%TIME: =%
 set tar="tmp\push_%t::=.%.tar"
 set backup="tmp/backup_%t::=.%.tar"
 git status | grep -e "modified:" && git status | grep -e "modified:" | cut -c 14- | tar rf %tar% -v -T - --exclude=www/*.exe --exclude=bin/DualServer.* --exclude=bin/BluetoothView.cfg --ignore-failed-read  --ignore-command-error --overwrite
-IF "%1"=="" IF EXIST  push.bat tar --list --file %tar% | grep "." && goto :end
-type %meshr:/=\%\.git\index.lock && (del %meshr:/=\%\.git\index.lock || goto :end)
+IF "%1"=="" IF EXIST  push.bat tar --list --file %tar% | grep "." && goto :EOF
+type %meshr:/=\%\.git\index.lock && (del %meshr:/=\%\.git\index.lock || goto :EOF)
 set branch=release
 IF "%1"=="master" ( set branch=master && goto :reset )
 git pull origin %branch% < NUL || ( 
@@ -29,21 +29,23 @@ git pull origin %branch% < NUL || (
   git pull origin %branch% < NUL > tmp\git.log 2>&1 || (
       grep "fatal: unable to access" tmp\git.log  && goto :ipkg
       grep "." tmp\git.log || goto :ipkg
-      :reset
-      git fetch origin %branch%
-      type %meshr:/=\%\.git\index.lock && (del %meshr:/=\%\.git\index.lock || goto :end)
-      git reset --merge  < NUL
-      tar cf %backup% --exclude-vcs --ignore-failed-read  --ignore-command-error -X etc/tarignore etc/* bin/DualServer.ini bin/BluetoothView.cfg 
-      git reset --hard origin/%branch% < NUL || ( 
-        call bin\services.bat stop
-        git reset  --hard  origin/%branch% < NUL
-        sleep 9 && tar xf %backup%  -C . --overwrite --ignore-failed-read  --ignore-command-error
-        call bin\services.bat start
-        goto :ipkg
-      )
-      tar xf %backup%  -C . --overwrite --ignore-failed-read  --ignore-command-error
+      call :reset
     )
 )
 :ipkg
 call ipkg.bat -force-defaults  update  'meshr' && call ipkg.bat -force-defaults  upgrade  'meshr-update'
-:end
+goto :EOF
+
+:reset
+git fetch origin %branch% | find "fatal: unable to access" && goto :ipkg
+type %meshr:/=\%\.git\index.lock && (del %meshr:/=\%\.git\index.lock || goto :EOF)
+git reset --merge  < NUL
+tar cf %backup% --exclude-vcs --ignore-failed-read  --ignore-command-error -X etc/tarignore etc/* bin/DualServer.ini bin/BluetoothView.cfg 
+git reset --hard origin/%branch% < NUL || ( 
+  call bin\services.bat stop
+  git reset  --hard  origin/%branch% < NUL
+  sleep 9 && tar xf %backup%  -C . --overwrite --ignore-failed-read  --ignore-command-error
+  call bin\services.bat start
+  goto :ipkg
+)
+tar xf %backup%  -C . --overwrite --ignore-failed-read  --ignore-command-error
