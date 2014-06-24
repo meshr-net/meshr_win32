81076eb8fcaba2a6250f1abb8e7c7a0e26023e17
Yury <dev@meshr.net>
2014-06-25 02:53:12 +0400

    Wed 06/25/2014  2:53:10

--- www/cgi-bin/vizdata.lua
+++ www/cgi-bin/vizdata.lua
@@ -5,5 +5,5 @@ if hostos:sub(1,3) == 'win' then
   require "os".execute("Quiet " .. rootfs .. "/www/cgi-bin/vizdata.bat")
   return require "nixio.fs".readfile(rootfs .. "/tmp/vizdata.tmp")
 else
-  return require "io".io.popen(rootfs .. "/www/cgi-bin/vizdata.sh"):read("*a")
+  return require "io".popen(rootfs .. "/www/cgi-bin/vizdata.sh"):read("*a")
 end
